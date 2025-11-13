af76ff7c594e6555c867cc73696b2f787378e212
kalpeshgithubrit <100852743+kalpeshgithubrit@users.noreply.github.com>
2025-11-13 21:37:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstate-bucket"
+    key = "terrform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+module "main" {
+  source = "module location"
+}
+
